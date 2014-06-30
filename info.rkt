fdcf720fd185b595cae892bcefbaa68a72837c3e
Greg Hendershott <greghendershott@gmail.com>
2014-06-29 20:06:53 -0400

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang setup/infotab
+(define version "0.1")
+(define deps '("base"
+               "parsack"
+               "rackjure"
+               "rackunit-lib"))
+(define build-deps '("at-exp-lib"
+                     "rackunit-lib"))
